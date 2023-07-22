3e12f7e7100d57c5c6d19853cf37d2f4aac42c7e
Fred <140276828+Fr-ed@users.noreply.github.com>
2023-07-22 20:49:11 +0100

    Create finance.js
    financial functions

---
+++ finance.js
@@ -0,0 +1,19 @@
+/**
+ * This JavaScript function provides a list of common financial functions.
+ * 
+ * @returns {Array} An array of common financial functions.
+ */
+function getCommonFinancialFunctions() {
+  return [
+    'calculateCompoundInterest',
+    'calculateLoanPayment',
+    'calculateFutureValue',
+    'calculatePresentValue',
+    'calculateNetPresentValue',
+    'calculateInternalRateOfReturn',
+    'calculateReturnOnInvestment',
+    'calculatePaybackPeriod',
+    'calculateDiscountedCashFlow',
+    'calculateEarningsPerShare'
+  ];
+}
