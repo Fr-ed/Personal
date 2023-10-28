a6a7f1b5bba5ab296e9812dddc186cb7c3016377
Fred <140276828+Fr-ed@users.noreply.github.com>
2023-10-28 14:47:19 +0100

    Create logging.js
    logging

---
+++ logging.js
@@ -0,0 +1,11 @@
+/**
+ * Logs a message to the console.
+ *
+ * @param {string} message - The message to be logged.
+ */
+function consoleLog(message) {
+    console.log(message);
+}
+
+// Usage Example
+consoleLog("Hello, world!");
